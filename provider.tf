43fee3abed8d7546444d4ba01ae172e5a9d50670
Amir-Mohammadi <115705598+AmirMohammadi60@users.noreply.github.com>
2023-05-09 20:29:16 +0200

    kommit provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,4 @@
+#  Der Provider ermöglicht es Terraform, mit Azure-Ressourcen zu interagieren und diese zu verwalten.
 terraform {
   required_providers {
     azurerm = {
@@ -5,7 +6,9 @@ terraform {
       version = "3.25.0"
     }
   }
-
+  
+# Diese Backend-Konfiguration sagt Terraform, dass der Azure Storage Account mit dem Namen "statefile25" 
+# und dem zugehörigen Container "statefile" verwendet werden soll,
   backend "azurerm" {
     resource_group_name  = "amir-rg-statefile"
     storage_account_name = "statefile25"
@@ -13,6 +16,9 @@ terraform {
     key                  = "terraform.tfstate"
   }
 }
+
+# definiert einen Provider für Terraform, speziell den "azurerm"-Provider, der verwendet wird, um mit Azure-Ressourcen zu 
+# interagieren und sie zu verwalten.
 provider "azurerm" {
   subscription_id = "ea6e6692-4d05-4c5b-9909-51c7dc5f5c2b"
   tenant_id       = "4dfdfd67-3a37-4e2e-b9f0-434c7061ba33"
